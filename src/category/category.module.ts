d83c521509830447bcb4f247f324f862dff4734f
Joe-Wando <jonathan6wando@gmail.com>
2026-07-02 17:44:10 +0200

    Ajout des elements avant le test

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { Category } from './category.entity';
+import { Product } from 'src/product/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Product, Category])],
   providers: [CategoryService],
   controllers: [CategoryController],
   exports: [TypeOrmModule],
